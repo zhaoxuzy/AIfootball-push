75bbb1f752e3c7438aa22a31e4719a95f51c68f6
Landon <84143557+zhaoxuzy@users.noreply.github.com>
2026-09-03 12:02:52 +0800

    Update environment.py

--- src/collectors/environment.py
+++ src/collectors/environment.py
@@ -1,11 +1,23 @@
+from src.utils import fetch_json, now_str
+from src.config import OPENWEATHER_API_KEY
+
 def collect_environment(match):
+    """天气、主裁判、主教练、排名等"""
+    weather = {}
+    if OPENWEATHER_API_KEY:
+        city = match.get('home_team', '')
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}&units=metric&lang=zh_cn"
+        data = fetch_json(url)
+        if data and 'weather' in data:
+            weather = {"天气": data['weather'][0]['description'], "温度": data['main']['temp']}
     return {
-        "天气": None,
-        "比赛城市": None,
-        "比赛场地": None,
-        "场地类型": None,
-        "主裁判": {"姓名": None, "场均黄牌": None, "场均点球": None, "执法风格": None, "本赛季执法场次": None},
+        "天气": weather,
+        "主裁判": {"姓名": None, "场均黄牌": None, "场均点球": None, "执法风格": None},
+        "主教练": {},
         "未来赛程": {"主队": [], "客队": []},
-        "积分排名": {"主队": None, "客队": None},
-        "德比属性": None
+        "积分排名": {},
+        "德比属性": "无",
+        "场地": None,
+        "来源": "手动/API",
+        "获取时间": now_str()
     }
